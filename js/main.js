d2c554f9bac6912b00b7c031bfface0b0a79d212
Maria Jose Serrano Higueras <mariajoseserranohigueras@MacBook-Air-de-Maria-Jose.local>
2025-10-22 11:07:48 +0200

    conts changed for let and kitten names upper cases

--- js/main.js
+++ js/main.js
@@ -10,13 +10,15 @@ const kittenSiamesDesc =
   "Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.";
 
 const kittenSiamesBreed = "Siames";
-const kittenSiamesName = "Anastacio";
+let kittenSiamesName = "Anastacio";
+const kittenSiamesNameUpper = kittenSiamesName.toUpperCase();
+
 const kittenSiamesImg = "https://dev.adalab.es/gato-siames.webp";
 
 const kittenSiames = `<li class="card">
           <article>
             <img class="card_img" src=${kittenSiamesImg} alt="siames-cat" />
-            <h3 class="card_title">${kittenSiamesName}</h3>
+            <h3 class="card_title">${kittenSiamesNameUpper}</h3> 
             <h4 class="card_race">${kittenSiamesBreed}</h4>
             <p class="card_description">
               ${kittenSiamesDesc}
@@ -24,34 +26,36 @@ const kittenSiames = `<li class="card">
           </article>
         </li>`;
 
-const kittenSphynx = `<li class="card">
-          <img class="card_img" src="https://dev.adalab.es/sphynx-gato.webp" alt="sphynx-cat" />
-          <h3 class="card_title">Fiona</h3>
-          <h4 class="card_race">Sphynx</h4>
-          <p class="card_description">
-            Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
-            hasta con pinta de alienígena han llegado a definir a esta raza
-            gatuna que se caracteriza por la «ausencia» de pelo.
-          </p>
-        </li>`;
-
-const kittenMaine = `
- <li class="card">
-          <img class="card_img" src="https://dev.adalab.es/maine-coon-cat.webp" alt="maine-coon-cat" />
-          <h3 class="card_title">Cielo</h3>
-          <h4 class="card_race">Maine Coon</h4>
-          <p class="card_description">
-            Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
-            bella mirada se ha convertido en una de sus señas de identidad.
-            Sus ojos son grandes y las orejas resultan largas y en punta.
-          </p>
-        </li>`;
-
+const kittenSphynxBreed = "Sphynx";
+const kittenSphynxName = "Fiona";
+const kittenSphynxImg = "https://dev.adalab.es/sphynx-gato.webp";
 const kittenSphynxDesc =
   "Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.";
 
+const kittenSphynx = `<li class="card">
+          <img class="card_img" src=${kittenSphynxImg} alt="sphynx-cat" />
+          <h3 class="card_title">${kittenSphynxName}</h3>
+          <h4 class="card_race">${kittenSphynxBreed}</h4>
+          <p class="card_description">
+          ${kittenSphynxDesc}
+          </p>
+        </li>`;
+
+const kittenMaineBreed = "Maine";
+const kittenMaineName = "Cielo";
 const kittenMaineDesc =
   "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.";
+const kittenMaineImg = "https://dev.adalab.es/maine-coon-cat.webp";
+
+const kittenMaine = `
+ <li class="card">
+          <img class="card_img" src=${kittenMaineImg} alt="maine-coon-cat" />
+          <h3 class="card_title">${kittenMaineName}</h3>
+          <h4 class="card_race">${kittenMaineBreed}</h4>
+          <p class="card_description">
+          ${kittenMaineDesc}
+          </p>
+        </li>`;
 
 jsList.innerHTML = kittenSiames + kittenSphynx + kittenMaine;
 
